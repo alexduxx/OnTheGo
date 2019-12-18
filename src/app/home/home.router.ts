7472cbfa8a1bb5231cfe2e7b1eb50bfb8ec1067e
Alexandru Duce <alexduxx@gmail.com>
2019-12-18 20:03:17 +0100

    doing the routing

---
+++ src/app/home/home.router.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home.page';
+
+const routes: Routes = [
+    {
+        path: 'home',
+        component: HomePage,
+        children: [
+            {
+                path: 'shop-list',
+                loadChildren: () => import('../shop-list/shop-list.module').then(m => m.ShopListPageModule)
+            },
+            {
+                path: 'orders',
+                loadChildren: () => import('../orders/orders.module').then(m => m.OrdersPageModule)
+            },
+            {
+                path: 'profile',
+                loadChildren: () => import('../profile/profile.module').then(m => m.ProfilePageModule)
+            },
+        ]
+    },
+    {
+        path: '',
+        redirectTo: 'home/shop-list',
+        pathMatch: 'full'
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+
+export class HomeRouter { }
